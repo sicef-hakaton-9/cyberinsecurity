650926d42c04a36ac841911a135690c8f493e5f8
Sara <saraprus@elfak.rs>
2023-11-04 20:17:22 +0100

    landing page done

--- frontend/grayFront/src/app/app.module.ts
+++ frontend/grayFront/src/app/app.module.ts
@@ -6,19 +6,29 @@ import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { RouterModule } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CommentComponent } from './comment/comment.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-    LogInComponent
+    LogInComponent,
+    NavBarComponent,
+    CommentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       { path: 'sign-up', component: SignUpComponent },
-      { path: 'log-in', component: LogInComponent }
+      { path: 'log-in', component: LogInComponent },
+      { path: '', redirectTo: '/log-in', pathMatch: 'full'},
+      { path:'nav-bar', component: NavBarComponent},
+      { path:'comment', component: CommentComponent},
+      { path:'home', component: HomeComponent}
     ])
   ],
   providers: [],
---
+++ frontend/grayFront/src/app/comment/comment.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.css']
+})
+export class CommentComponent {
+  showModal = false;
+  toggleModal(){
+    this.showModal = !this.showModal;
+  }
+}
