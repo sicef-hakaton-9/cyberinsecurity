49fb77e715e07ab2009166459c89c4c5dfa168f5
Sara <saraprus@elfak.rs>
2023-11-04 16:34:22 +0100

    auth'

--- frontend/grayFront/src/app/app.module.ts
+++ frontend/grayFront/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'sign-up', component: SignUpComponent },
+      { path: 'log-in', component: LogInComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/grayFront/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js, ts}"],
+  theme: {
+    extend: {
+      colors: {
+        rllyWhite: "#fafafa",
+        white: "#f4f4f5",
+        ligthGrey: "#e4e4e7",
+        midGrey: "#d4d4d8",
+        darkGrey: "#a1a1aa",
+        ligthBlack: "#27272a",
+        black: "#09090b",
+      },
+    },
+  },
+  plugins: [],
+};
